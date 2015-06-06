421981f81d8637d990213012040a5c993e13e90b
Kento Masukawa <masukawa@west.sd.keio.ac.jp>
2015-06-06 11:34:12 +0900

    spec3: イテレータを用いて仕様3を実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    std::cout << "オムライス\n";
+//イテレータを用いる
+    vector<string> recipe;
+    vector<string> ::iterator it;
+
+//recipeにデータを読み出して入れる
+    ifstream recipes(argv[1]);
+    string str;
+    while (getline(recipes,str)){
+        recipe.push_back(str);
+    }
+
+//recipeの最初から最後まで出力
+    for(it = recipe.begin();it != recipe.end();it++){
+        cout << *it << endl;
+    }
+    
     return 0;
 }
